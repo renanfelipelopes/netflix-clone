74e49718bd95cb2c804b37eb675ae3c56d65bf24
Renan Lopes <renanlopes.ads@gmail.com>
2021-09-25 22:43:38 -0300

    project-clone

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './index.css';
+
+const Header = ({black}) => {
+    return (
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-5.png" alt="Netflix"></img>
+                </a>
+            </div>
+            <div className="header--user">
+                <a href="/">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Usuário"></img>
+                </a>                
+            </div>
+        </header>
+    )
+}
+
+export default Header;
